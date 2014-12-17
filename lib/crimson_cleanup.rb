9b4266d85f11da92d8278036d131077ffb8c40a3
Fred Ancheta <fred@navigatingcancer.com>
2014-12-17 15:17:15 -0800

    WIP

--- lib/crimson_cleanup.rb
+++ lib/crimson_cleanup.rb
@@ -1,26 +1,28 @@
 class CrimsonCleanup
-  RETRY_CONDITIONS = [
-    Proc.new { |job| job['exception'] == 'Net::HTTPBadResponse' },
-    Proc.new { |job| job['exception'] == 'Resque::DirtyExit' },
-    Proc.new { |job| job['exception'] == 'NoMemoryError' },
-    Proc.new { |job| job['exception'] == 'EOFError' },
-    Proc.new { |job| job['exception'] == 'Errno::ECONNRESET' },
-    Proc.new { |job| job['exception'] == 'Errno::EPIPE' },
-    Proc.new { |job| job['exception'] == 'ActiveRecord::DeadlockVictim' },
-    Proc.new { |job| job['error'].include?('timed out') },
-    Proc.new { |job| job['error'].include?('Cannot allocate memory') },
-    Proc.new { |job| job['error'].include?('Adaptive Server is unavailable') },
-  ]
+  class << self
+    RETRY_CONDITIONS = [
+      Proc.new { |job| job['exception'] == 'Net::HTTPBadResponse' },
+      Proc.new { |job| job['exception'] == 'Resque::DirtyExit' },
+      Proc.new { |job| job['exception'] == 'NoMemoryError' },
+      Proc.new { |job| job['exception'] == 'EOFError' },
+      Proc.new { |job| job['exception'] == 'Errno::ECONNRESET' },
+      Proc.new { |job| job['exception'] == 'Errno::EPIPE' },
+      Proc.new { |job| job['exception'] == 'ActiveRecord::DeadlockVictim' },
+      Proc.new { |job| job['error'].include?('timed out') },
+      Proc.new { |job| job['error'].include?('Cannot allocate memory') },
+      Proc.new { |job| job['error'].include?('Adaptive Server is unavailable') },
+    ]
 
-  def clean!
-    RETRY_CONDITIONS.each do |condition|
-      cleaner.requeue(true) { |job| condition.call(job) }
+    def clean!
+      RETRY_CONDITIONS.each do |condition|
+        cleaner.requeue(true) { |job| condition.call(job) }
+      end
     end
-  end
 
-  private
+    private
 
-  def cleaner
-    @cleaner ||= Resque::Plugins::ResqueCleaner.new
+    def cleaner
+      @cleaner ||= Resque::Plugins::ResqueCleaner.new
+    end
   end
 end
